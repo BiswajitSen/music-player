4a8fca53a0b5e19a8498d701eb9afb96f12c5682
Biswajit Sen <biswajitsen@Biswajits-MacBook-Pro.local>
2024-03-09 15:21:25 +0530

    Initial commit

---
+++ resources/scripts/index.js
@@ -0,0 +1,10 @@
+function getSearchBar() {
+    return document.getElementById('search-bar');
+}
+
+function main() {
+    console.log("Hello World");
+    console.log(getSearchBar());
+}
+
+window.onload = main;
